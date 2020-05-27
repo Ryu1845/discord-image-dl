bd7ddabd979456ae0991c2054db12403dd2840bd
sofian18 <38885373+sofian18@users.noreply.github.com>
2020-05-27 16:23:33 +0200

    main file

---
+++ discord-image-dl.py
@@ -0,0 +1,18 @@
+import discord, asyncio
+import os
+
+token=""
+client = discord.Client()
+print('Enter channelID')
+channelID=int(input())
+@client.event
+async def on_connect():
+    channel=client.get_channel(channelID)
+    if not os.path.exists(channel.name):
+        os.mkdir(channel.name)
+    os.chdir(channel.name)
+    async for message in channel.history():
+        for attachment in message.attachments:
+            await attachment.save(attachment.filename)
+            print(attachment.filename)
+client.run(token, bot=False)
