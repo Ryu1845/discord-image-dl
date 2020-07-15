4fd16707ab3c7363205dc5e767a95a495c694300
Ryu18 <38885373+Ryu184@users.noreply.github.com>
2020-07-15 17:27:29 +0200

    Update discord-image-dl.py
    Now downloads image embeds

--- discord-image-dl.py
+++ discord-image-dl.py
@@ -1,5 +1,6 @@
 import discord, asyncio
 import os
+import requests
 
 token=""
 client = discord.Client()
@@ -17,6 +18,14 @@ async def on_connect():
         for attachment in message.attachments:
             await attachment.save(attachment.filename)
             print(attachment.filename)
+        for embed in message.embeds:
+            if embed.image.url is not embed.Empty:
+                image_url=embed.image.url
+                filename = image_url.split('/')[-1]
+                print(filename)
+                img_data = requests.get(image_url).content
+                with open(filename, 'wb') as handler:
+                    handler.write(img_data)             
     loop=asyncio.get_event_loop()
     loop.stop()
 client.run(token, bot=False)
