ff8db04caf61041717fb1bbde61d3c61f1bde743
Ryu18 <38885373+Ryu184@users.noreply.github.com>
2020-07-15 21:25:46 +0200

    Same but better

--- discord-image-dl.py
+++ discord-image-dl.py
@@ -1,28 +1,28 @@
 import discord, asyncio
 import os
 import requests
-wd=os.getcwd()
+
 token=""
 client = discord.Client()
+wd=os.getcwd()
 print('Enter channelID')
 channelID=int(input())
-if not os.path.exists(str(channelID)+".history"):
-    f=open(str(channelID)+".history","w")
-    f.close()
-file1=open(str(channelID)+".history","r") 
-history1=file1.readlines()
-file1.close()
+if not os.path.exists(f"{channelID}.history"):
+    os.mknod(f"{channelID}.history")
+with open(f"{channelID}.history", "r") as f:
+    history1 = f.readlines()
+
 @client.event
 async def on_connect():
     print('-------------------')
     channel=client.get_channel(channelID)
-    print('Downloading #'+channel.name+'\n')
+    print(f"Downloading #{channel.name}")
     if not os.path.exists(channel.name):
         os.mkdir(channel.name)
     os.chdir(channel.name)
     async for message in channel.history():
-        file1 = open(wd+"/"+str(channelID)+".history","a")
-        if not str(message.id)+"\n" in history1: 
+        file1 = open(os.path.join(wd, f"{channelID}.history"))
+        if not f"{message.id}\n" in history1: 
             for attachment in message.attachments:
                 print(attachment.url)
                 await attachment.save(str(message.id))
@@ -35,7 +35,7 @@ async def on_connect():
                     img_data = requests.get(image_url).content
                     with open(filename, 'wb') as handler:
                         handler.write(img_data)
-            file1.write(str(message.id)+"\n")             
+            file1.write(f"{message.id}\n")             
     loop=asyncio.get_event_loop()
     loop.stop()
     file1.close()
